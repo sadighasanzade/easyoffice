6986265eed45ee699c5e0db21dfad08d734598e5
sadighasanzade <sadiqhesensr@gmail.com>
2023-12-25 02:42:08 +0200

    removed jitpack.yml

--- easyoffice/build.gradle.kts
+++ easyoffice/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components.findByName("release"))
                 groupId = "com.github.sadighasanzade"
                 artifactId = "easy-office"
-                version = "1.5"
+                version = "1.6"
 
             }
 
